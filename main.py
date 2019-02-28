d9e683b23b2c094914bc628b5996871020288ea9
SEric <sch.kerner@gmail.com>
2019-02-28 10:05:09 +0100

    Second Commit

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from kivy.app import App
 
-meineAnwendung = App()
+class RechnerApp(App):
+    pass
+
+    def berechne(self,button, gui, app):
+        print("berechne wird ausgeführt")
+        for x in [self, button, gui, app]:
+            print(x)
+        button.text = "fertig berechnet!"
+        print("fertig")
+
+    def button_pressed(self, button):
+        print(button.text)
+        if self.root.ids.Feld.text == "Hallo":
+            self.root.ids.Feld.text = button.text
+        else:
+            self.root.ids.Feld.text += button.text
+        print(self.root.ids)
+
+meineAnwendung = RechnerApp()
 print(meineAnwendung)
 meineAnwendung.run()
